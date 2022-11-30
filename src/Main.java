d5f3641fd45700f04af948adf56b325740136bf8
qmar <video4den@gmail.com>
2022-11-30 15:55:31 +0500

    задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Домашнее задание. Переменные 1");
+        task1();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
+
     }
 }
