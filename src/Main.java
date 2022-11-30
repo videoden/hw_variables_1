93675d5519baecca8a2af94d9121b0157dc6412d
qmar <video4den@gmail.com>
2022-11-30 21:24:40 +0500

    добавил задание 4 и 5

--- src/Main.java
+++ src/Main.java
@@ -13,33 +13,34 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println();
 
         System.out.println("Задача 2");
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
-
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println();
 
         System.out.println("Задача 3");
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
-
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println();
 
         System.out.println("Задача 4");
-
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        System.out.println();
 
         System.out.println("Задача 5");
         var frog = 3.5;
@@ -50,7 +51,36 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+        System.out.println();
 
+        System.out.println("Задача 6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println("первый боксёр весит " + boxer1);
+        System.out.println("второй боксёр весит " + boxer2);
+        var totalWeight = boxer1 + boxer2;
+        System.out.println("общий вес боксёров " + totalWeight);
+        var weightDifference = boxer2 - boxer1;
+        System.out.println("разница в весе боксёров " + weightDifference);
+
+        System.out.println("Задача 7");
+        var weightDifference1 = boxer2 - boxer1;
+        System.out.println("разница в весе боксёров " + weightDifference1);
+        var weightDifference2 = boxer2 % boxer1;
+        System.out.println("разница в весе боксёров " + weightDifference2);
+        System.out.println();
+
+        System.out.println("Задача 8.1");
+        var totalHours = 640;
+        var hoursPerPerson = 8;
+        var totalPerson = totalHours / hoursPerPerson;
+        System.out.println("Всего работников в компании - " + totalPerson + " человек" );
+        System.out.println();
+
+        System.out.println("Задача 8.2");
+        var totalPerson2 = totalPerson + 94;
+        var hoursPerPerson2 = totalHours / totalPerson2;
+        System.out.println("Если в компании работает " + totalPerson2 +" человека, то всего " + hoursPerPerson2 + " часа работы может быть поделено между сотрудниками");
 
 
 
